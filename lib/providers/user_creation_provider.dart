ae480a73f2fd43a21463a74a4f9d17f32c6ecfc8
Juampi Q <juampiq6@gmail.com>
2025-05-12 23:28:29 -0300

    fix: add autoDispose to userCreationProvider

--- lib/providers/user_creation_provider.dart
+++ lib/providers/user_creation_provider.dart
@@ -3,7 +3,7 @@ import 'package:splittymate/models/dto/user_creation_dto.dart';
 import 'package:splittymate/providers/supabase_service_provider.dart';
 
 final userCreationProvider =
-    FutureProvider.family<bool, UserCreationDTO>((ref, dto) async {
+    FutureProvider.family.autoDispose<bool, UserCreationDTO>((ref, dto) async {
   await ref.read(supabaseProvider).createUser(dto);
   return true;
 });
