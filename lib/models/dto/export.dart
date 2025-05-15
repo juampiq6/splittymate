8ab53a8454f91f2031602f86aea01b9e29eac2a9
Juampi Q <juampiq6@gmail.com>
2025-05-15 16:00:35 -0300

    feat: add export files for DTOs and models in the lib/models directory

---
+++ lib/models/dto/export.dart
@@ -0,0 +1,3 @@
+export 'group_creation_dto.dart';
+export 'transaction_creation_dto.dart';
+export 'user_creation_dto.dart';
---
+++ lib/models/export.dart
@@ -0,0 +1,5 @@
+export 'user.dart';
+export 'transactions/exports.dart';
+export 'currency.dart';
+export 'split_group.dart';
+export 'debt.dart';
