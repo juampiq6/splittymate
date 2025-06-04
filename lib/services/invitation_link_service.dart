6a8c694e2065f46d6c1f9ea2095f020825626d03
Juampi Q <juampiq6@gmail.com>
2025-06-04 18:32:48 -0300

    chore: remove outdated comment regarding future backend implementation in InvitationLinkService

--- lib/services/invitation_link_service.dart
+++ lib/services/invitation_link_service.dart
@@ -4,7 +4,6 @@ import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:splittymate/env_vars.dart';
 import 'package:splittymate/services/interfaces/export.dart';
 
-// THIS SERVICE SHOULD BE IMPLEMENTED AS A BACKEND FUNCITON IN THE FUTURE
 class InvitationLinkService {
   final FaaServiceInterface functionService;
 
